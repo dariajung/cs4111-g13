bf8edc95a9f9aef5213492fccfe53906749e9a7c
Daria Jung <djj2115cu@gmail.com>
2016-02-27 15:53:19 -0500

    switch strings to varchar if they can be varied in length

--- Tables.sql
+++ Tables.sql
@@ -1,37 +1,37 @@
 CREATE TABLE Politicians(
-	name char(50),
+	name varchar(50),
 	DOB date,
 	net_worth real,
 	incumbent_status boolean,
-	party_affiliation char(20),
+	party_affiliation varchar(30),
 	years_in_office integer,
 	PRIMARY KEY (name, DOB));
 
 CREATE TABLE Senators(
-	name char(50),
+	name varchar(50),
 	DOB date,
 	PRIMARY KEY(name, DOB),
 	FOREIGN KEY(name, DOB) REFERENCES Politicians(name, DOB) ON DELETE CASCADE);
 
 CREATE TABLE Representatives(
-	name char(50),
+	name varchar(50),
 	DOB date,
 	PRIMARY KEY(name, DOB),
 	FOREIGN KEY(name, DOB) REFERENCES Politicians(name, DOB) ON DELETE CASCADE); 
 
 CREATE TABLE Industries(
-	summary char(300),
+	summary varchar(300),
 	PRIMARY KEY (summary));
 
 CREATE TABLE Legislation(
-	name char(100),
+	name varchar(100),
 	passed boolean NOTNULL, /* A Legislation must have either passed or failed */
 	PRIMARY KEY (name));
 
 CREATE TABLE Super_PACs(
 	committee_id char(11),
-	name char(50),
-	viewpoint char(15),
+	name varchar(50),
+	viewpoint varchar(30),
 	budget real,
 	cash_spent real,
 	PRIMARY KEY (committee_id),
@@ -39,7 +39,7 @@ CREATE TABLE Super_PACs(
 
 CREATE TABLE PACs(
 	committee_id char(11),
-	name char(50),
+	name varchar(50),
 	budget real,
 	cash_spent real,
 	cash_on_hand real,
@@ -48,15 +48,15 @@ CREATE TABLE PACs(
 	UNIQUE (name));
 
 CREATE TABLE Rep_State(
-	state_name char(15),
-	major_ethnicity char(50),
+	state_name varchar(15),
+	major_ethnicity varchar(50),
 	median_age real,
-	major_party char(50),
+	major_party varchar(50),
 	median_income real,
 	poverty_level real,
-	sr_senator_name char(50) NOTNULL,
+	sr_senator_name varchar(50) NOTNULL,
 	sr_senator_DOB date NOTNULL,
-	jr_senator_name char(50) NOTNULL,
+	jr_senator_name varchar(50) NOTNULL,
 	jr_senator_DOB date NOTNULL,
 	FOREIGN KEY (sr_senator_name, sr_senator_DOB) REFERENCES Senators(name, DOB) ON DELETE NO ACTION,
 	FOREIGN KEY (jr_senator_name, jr_senator_DOB) REFERENCES Senators(name, DOB) ON DELETE NO ACTION,
@@ -66,14 +66,14 @@ CREATE TABLE Rep_State(
 
 
 CREATE TABLE Rep_District(
-	state_name char(15),
+	state_name varchar(15),
 	district_number integer,
-	major_ethnicity char(50),
+	major_ethnicity varchar(50),
 	median_age real,
-	major_party char(50),
+	major_party varchar(50),
 	median_income real,
 	poverty_level real,
-	representative_name char(50) NOTNULL,
+	representative_name varchar(50) NOTNULL,
 	representative_DOB date NOTNULL,
 	FOREIGN KEY(name, DOB) REFERENCES Representatives(name, DOB)
 	ON DELETE NO ACTION,
@@ -81,16 +81,16 @@ CREATE TABLE Rep_District(
 	PRIMARY KEY (state_name, district_number));	
 
 CREATE TABLE Advocates(
-	summary char(300),
-	name char(100),
+	summary varchar(300), /* summary of a cause/industry */
+	name varchar(100), /* name of legislation */
 	FOREIGN KEY (summary) REFERENCES Industries(summary)
 	ON DELETE CASCADE,
 	FOREIGN KEY (name) REFERENCES Legislation(name) ON DELETE CASCADE,
 	PRIMARY KEY (name, summary));
 
 CREATE TABLE P_Sponsors(
-	legislation_name char(100),
-	p_sponsor_name char(50),
+	legislation_name varchar(100),
+	p_sponsor_name varchar(50),
 	p_sponsor_DOB date,
 	FOREIGN KEY (p_sponsor_name, p_sponsor_DOB) REFERENCES Politicians(name, DOB) ON DELETE CASCADE,
 	FOREIGN KEY (legislation_name) REFERENCES Legislation(name) ON DELETE CASCADE,
@@ -99,7 +99,7 @@ CREATE TABLE P_Sponsors(
 
 CREATE TABLE Interested_In(
 	committee_id char(11),
-	industry_summary char(300),
+	industry_summary varchar(300),
 	PRIMARY KEY(committee_id, industry_summary),
 	FOREIGN KEY (committee_id) references PACs(committee_id) ON DELETE CASCADE,
 	FOREIGN KEY (industry_summary) references Industries(summary) ON DELETE CASCADE); 
@@ -107,7 +107,7 @@ CREATE TABLE Interested_In(
 
 CREATE TABLE SPAC_Supports(
 	committee_id char(11),
-	name char(100),
+	name varchar(100),
 	DOB date,
 	amount real,
 	FOREIGN KEY (committee_id) REFERENCES Super_PACs(committee_id) ON DELETE CASCADE,
@@ -123,7 +123,7 @@ CREATE TABLE PAC_Donate(
 
 CREATE TABLE PAC_Supports(
 	committee_id char(11),
-	politician_name char(50),
+	politician_name varchar(50),
 	politician_DOB date,
 	amount real,
 	FOREIGN KEY (politician_name, politician_DOB) REFERENCES Politicians(name, DOB) ON DELETE CASCADE,
