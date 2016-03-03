a508b590f4749192f9f7f8b68ffe4f633208b410
Daria Jung <djj2115cu@gmail.com>
2016-03-03 12:36:38 -0500

    last query

--- interesting_queries.sql
+++ interesting_queries.sql
@@ -20,7 +20,7 @@ WHERE p.name = ps.politician_name AND pacs.committee_id = ps.committee_id
 GROUP BY p.party_affiliation;
 
 
--- Show average median income of states grouped by their senior senators' party affiliation where percentage minority > 10
+-- Show average median income of states where percentage minority > 10 grouped by their senior senators' party affiliation 
 SELECT p.party_affiliation, AVG(rep_state.median_income)
 FROM politicians p, rep_state
 WHERE rep_state.percentage_minorities > 10 AND (p.name = rep_state.sr_senator_name)
@@ -28,7 +28,18 @@ GROUP BY p.party_affiliation
 
 
 -- select all industries who donated over $1,000,000 USD to a politician
-SELECT i.summary, p.name, p.party_affiliation
-FROM politicians p, industries i, 
-WHERE 
-GROUP BY
+-- SELECT i.summary, p.name, p.party_affiliation
+-- FROM politicians p, industries i 
+-- WHERE 
+-- GROUP BY
+
+-- inner join pacs and pac_supports
+
+-- select all industries that have PACS that donated over $1,000,000 USD to a PAC that supports a politician
+SELECT DISTINCT i.industry_summary
+FROM (SELECT *
+FROM pac_supports INNER JOIN pac_donate ON pac_supports.committee_id = pac_donate.to_committee_id
+WHERE pac_supports.amount > 1000000) as new_t, interested_in i
+WHERE i.committee_id = new_t.from_committee_id
+
+
