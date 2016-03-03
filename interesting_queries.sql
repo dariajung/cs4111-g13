915cec2c9e748b365830114e61edda80348093ea
Daria Jung <djj2115cu@gmail.com>
2016-03-03 12:46:00 -0500

    add query that return committee_ids of PACs and their industries that have donated over $1,000,000 USD to a PAC that supports a politician

--- interesting_queries.sql
+++ interesting_queries.sql
@@ -27,19 +27,9 @@ WHERE rep_state.percentage_minorities > 10 AND (p.name = rep_state.sr_senator_na
 GROUP BY p.party_affiliation
 
 
--- select all industries who donated over $1,000,000 USD to a politician
--- SELECT i.summary, p.name, p.party_affiliation
--- FROM politicians p, industries i 
--- WHERE 
--- GROUP BY
-
--- inner join pacs and pac_supports
-
--- select all industries that have PACS that donated over $1,000,000 USD to a PAC that supports a politician
-SELECT DISTINCT i.industry_summary
+-- return committee_id of PAC and their industry that have donated over $1,000,000 USD to a PAC that supports a politician
+SELECT DISTINCT i.committee_id, i.industry_summary
 FROM (SELECT *
 FROM pac_supports INNER JOIN pac_donate ON pac_supports.committee_id = pac_donate.to_committee_id
 WHERE pac_supports.amount > 1000000) as new_t, interested_in i
 WHERE i.committee_id = new_t.from_committee_id
-
-
